0f8086ca651d0bbd2d67261cce38f084cdaeea3f
igorvlima <igorveraslima3@gmail.com>
2025-03-19 16:01:18 -0300

    first commit

---
+++ src/main/java/com/fastfood/api/FiapSoatTechChallengeSecondPhaseApplication.java
@@ -0,0 +1,13 @@
+package com.fastfood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapSoatTechChallengeSecondPhaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiapSoatTechChallengeSecondPhaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastfood/api/FiapSoatTechChallengeSecondPhaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastfood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapSoatTechChallengeSecondPhaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
